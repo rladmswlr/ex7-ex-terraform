7bf69d4522b192aa97b5e33462c41efa35ba7b3a
rladmswlr <alswndmswlr@naver.com>
2026-09-17 13:16:15 +0900

    error test now

--- infra/project/main.tf
+++ infra/project/main.tf
@@ -4,6 +4,10 @@ resource "aws_vpc" "this" {
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  assign_generated_ipv6_cidr_block     = false
+  enable_network_address_usage_metrics = fasle
+
   tags = {
     Name = "rladmswlr-test-vpc"
   }
